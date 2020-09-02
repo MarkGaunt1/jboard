f7dea96c250a7a01d2608cbce5f69ad1c5b5100b
MarkGaunt1 <marklgaunt@googlemail.com>
2020-09-02 18:36:41 +0100

    H start change

--- index.js
+++ index.js
@@ -19,7 +19,8 @@ mongoose.Promise = global.Promise;
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Method");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   next();
 });
 
